8ee2e1a306ec4496ba2f473ee789d414b54b9f9b
Nguyen Tan Dat <tandat081104@gmail.com>
2026-05-12 17:35:52 +0700

    feat: initialize NestJS application with Swagger, global validation pipes, and cookie-parser middleware

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 3069);
+  await app.listen(process.env.PORT ?? 3069, '0.0.0.0');
 }
 bootstrap();
